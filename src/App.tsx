5c6fbafd946356e5248ea72ac86ce0d39e6dee19
katleo rantle <kprantle@gmail.com>
2026-04-21 09:52:26 +0200

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='bg-amber-300 text-blue-800 text-4xl'>
+        welcome to my site
+      </h1>
+    </>
+  );
+}
+
+export default App
